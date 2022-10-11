c4644fec51fd3b857795497f667495f9b4df3d22
SamuelSolis11 <samuelsolis1183@gmail.com>
2022-10-11 09:02:24 -0500

    actualizacion

---
+++ API/Parcial 2/Modulo/modulo.js
@@ -0,0 +1,23 @@
+//const cadenas={}
+function pasarMayusculas(cadena){
+  return cadena.toUpperCase();
+}
+
+function quitarEspacios(cadena){
+    return cadena.trim();
+}
+
+function obtenerLongitud(cadena){
+    return cadena.length;
+}
+
+console.log(module);
+
+exports.pasarMayusculas=pasarMayusculas;
+exports.quitarEspacios=quitarEspacios;
+exports.obtenerLongitud=obtenerLongitud;
+
+console.log(module);
+
+  
+  
---
+++ API/Parcial 2/modulosm/modulo.js
@@ -0,0 +1,16 @@
+//const cadenas={}
+ export function pasarMayusculas(cadena){
+  return cadena.toUpperCase();
+}
+
+export function quitarEspacios(cadena){
+    return cadena.replace(/ /g,"");
+}
+
+export function obtenerLongitud(cadena){
+    return cadena.length;
+}
+
+
+  
+  
