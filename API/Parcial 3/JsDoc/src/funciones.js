6cc3351248d91dc22cf21b3a761f53a0a6c21504
SamuelSolis11 <samuelsolis1183@gmail.com>
2022-11-22 08:59:03 -0600

    se subio Jsdoc

---
+++ API/Parcial 3/JsDoc/src/funciones.js
@@ -0,0 +1,9 @@
+ /**
+  *  Esta funcion recibe un numero que elevar y lo va a regresar ya elevado a la potencia que es.
+  * @param {int} numerobase  Numero que se va elevar
+  * @param {int} exponente Potencia a la que se va elevar el numero
+  * 
+  */
+ export function potencia(numerobase,exponente){
+    return numerobase*exponente
+}
---
+++ API/Parcial 3/JsDoc/src/index.js
@@ -0,0 +1,3 @@
+import * as Funciones from './funciones.js';
+
+console.log(Funciones.potencia(2,2));
